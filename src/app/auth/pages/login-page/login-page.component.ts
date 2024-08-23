637a22f09b8e0c792fbbfbb2eef026a2726b1b58
jere <jere77lagos@gmail.com>
2024-08-23 16:39:02 +0200

    implement isAuthenticated guard

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
 import Swal from 'sweetalert2'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -12,11 +13,12 @@ import Swal from 'sweetalert2'
 export class LoginPageComponent {
 
   private fb = inject( FormBuilder );
-  private authService = inject( AuthService )
+  private authService = inject( AuthService );
+  private router = inject( Router );
 
   public myForm: FormGroup = this.fb.group({
-    email: ['', [Validators.required, Validators.email ]],
-    password: ['', [Validators.required, Validators.minLength(6) ]],
+    email: ['anna@mail.com', [Validators.required, Validators.email ]],
+    password: ['987654', [Validators.required, Validators.minLength(6) ]],
   });
 
   login() {
@@ -24,7 +26,7 @@ export class LoginPageComponent {
 
     this.authService.login(email, password)
       .subscribe({
-        next: () => console.log('everything fine'),
+        next: () => this.router.navigateByUrl('/dashboard'),
         error: (message) => {
           Swal.fire('Error', message, 'error')
         }
