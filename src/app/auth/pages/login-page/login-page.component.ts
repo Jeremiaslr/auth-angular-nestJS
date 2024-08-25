0ae97a9231517290025ee929c8f0bbb28e029539
jere <jere77lagos@gmail.com>
2024-08-25 13:32:31 +0200

    delete console log

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -17,8 +17,8 @@ export class LoginPageComponent {
   private router = inject( Router );
 
   public myForm: FormGroup = this.fb.group({
-    email: ['anna@mail.com', [Validators.required, Validators.email ]],
-    password: ['987654', [Validators.required, Validators.minLength(6) ]],
+    email: ['', [Validators.required, Validators.email ]],
+    password: ['', [Validators.required, Validators.minLength(6) ]],
   });
 
   login() {
